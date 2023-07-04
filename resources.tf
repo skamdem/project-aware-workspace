c152508263425c56866cb93eb6224e8ff70c33d2
Sylvain K <sylvain.kamdem@gmail.com>
2023-07-04 18:36:57 +0100

    removed the sqs queue

--- resources.tf
+++ resources.tf
@@ -6,16 +6,3 @@ resource "aws_s3_bucket" "my_s3_bucket" {
     Environment = local.environment_name
   }
 }
-
-resource "aws_sqs_queue" "my_sqs_queue" {
-  name                      = "${local.environment_name}-queue"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-
-  tags = {
-    Name        = "project_aware_workspace queue"
-    Environment = local.environment_name
-  }
-}
